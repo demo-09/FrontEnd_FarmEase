e96a6c446c532cca6d6747b8bc7933fdd285f776
Demo <ujjdemo@gmail.com>
2026-05-14 19:18:53 +0530

    Change BASE_URL from LOCAL_URL to LIVE_URL

--- src/app/core/api.config.ts
+++ src/app/core/api.config.ts
@@ -5,6 +5,6 @@ const LOCAL_URL = 'http://localhost:5009';
 
 // Check if we are running on localhost
 // FORCE LIVE URL EVERYWHERE
-export const BASE_URL = LOCAL_URL;
+export const BASE_URL = LIVE_URL;
 export const API_URL = `${BASE_URL}/api`;
 export const HUB_URL = `${BASE_URL}/chatHub`;
